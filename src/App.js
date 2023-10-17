29f112aab3fb91d949cc94fac15f067a6dc7a97b
Tanvir Hossen Raju <103171957+exploreTanvir@users.noreply.github.com>
2023-10-17 21:17:49 +0600

    Products listing App

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 import Card from "./Components/Card";
+import Data from "./Data.json";
 
 function App(){
+    let items=[]
+    items= Data.map((item)=>{
+        console.log(uuidv4())
+        const {id,ProductPic,Title,Price,Rating,Desc}=item
+       return <Card key={id} ProductImg={ProductPic} ProductTitle={Title} price={Price}  rating={Rating}  ProductDesc={Desc}/>
+    })
     return <div className="fullBody">
         <h1 className="headingCss">BD Store</h1>
-        {/* use of props for Card.js file */}
-
-        <div className="parant">
-
-        <div className="first">
-    <Card  todoTitle="Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops" price="Price : $109.95" rating="Rating : 3.9/5" todoDesc="Description : Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday" />
-
-        <Card todoTitle="Mens Casual Premium Slim Fit T-Shirts " price="Price : $22.3" rating="Rating : 4.1/5" todoDesc="Description : Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket." />
-
-        <Card todoTitle="Mens Cotton Jacket" price="Price : $55.99" rating="Rating : 4.7/5" todoDesc="Description : great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day." />
-        </div>
-
-        <div className="second">
-    <Card todoTitle="Mens Casual Slim Fit" price="Price : $15.99" rating="Rating : 2.1/5" todoDesc="The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description." />
-
-        <Card todoTitle="John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet" price="Price : $695" rating="Rating : 4.6/5" todoDesc="Description : From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection." />
-
-        <Card todoTitle="Solid Gold Petite Micropave " price="Price : $168" rating="Rating : 3.9/5" todoDesc="Description :  Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days." />
-        </div>
-        
-        </div>
-
-
+        {items}
+       
     </div>
 }
 
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,22 +1,22 @@
 function Card(props){
 
     // Props and destructuring
-    const {todoTitle,todoDesc,price,rating,image}=props
+    const {ProductTitle,ProductDesc,price,rating,ProductImg}=props
 
 
-    return <div className="main" >
-     
-    <div className='card'>
-    <img src={image} />
-     <p className='cardTitle'>{todoTitle}</p> {/*Using props from app.js file*/}
+    return <div  className='card'>
+     <div className="Phto">
+     <img className="ProductImg" src={ProductImg} alt="Product Phto" />
+    </div>
+     <div className="down">
+     <p className='cardTitle'>{ProductTitle}</p> 
      <p className='price'>{price}</p>
      <p className='rating'>{rating}</p>
-     <p className='cardDesc'>{todoDesc}</p> {/*Using props from app.js file*/}
-    <button className="button">Add to cart</button>
+     <p className='cardDesc'>{ProductDesc}</p> 
+     <button className="button">Add to cart</button>
+     </div>
+    
         </div>
-    </div>
-
-
  }
 
 
